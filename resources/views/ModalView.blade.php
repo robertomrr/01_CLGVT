65592982f0ec219e19d82467df3556fc3bebea5d
Roberto Moreira <robertomrr@yahoo.com.br>
2024-07-17 10:31:24 -0300

    Ajustes nas telas de endereço

--- resources/views/ModalView.blade.php
+++ resources/views/ModalView.blade.php
@@ -1,54 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <title>Modal Example</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <!-- Adicione um link para o estilo CSS da modal -->
-    <!-- <link rel="stylesheet" href="modal.css"> -->
-<style>
-    /* Estilo da modal */
-    .modal {
-        display: none;
-        position: fixed;
-        z-index: 1;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgb(0,0,0);
-        background-color: rgba(0,0,0,0.4);
-    }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+        <title>Modal Example</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Adicione um link para o estilo CSS da modal -->
+        <!-- <link rel="stylesheet" href="modal.css"> -->
+        <style>
+            /* Estilo da modal */
+            .modal {
+                display: none;
+                position: fixed;
+                z-index: 1;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                overflow: auto;
+                background-color: rgb(0,0,0);
+                background-color: rgba(0,0,0,0.4);
+            }
 
-    /* Estilo do conteúdo da modal */
-    .modal-content {
-        background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 80%;
-    }
+            /* Estilo do conteúdo da modal */
+            .modal-content {
+                background-color: #fefefe;
+                margin: 15% auto;
+                padding: 20px;
+                border: 1px solid #888;
+                width: 80%;
+            }
 
-    /* Estilo do botão de fechamento */
-    .close {
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-    }
+            /* Estilo do botão de fechamento */
+            .close {
+                color: #aaa;
+                float: right;
+                font-size: 28px;
+                font-weight: bold;
+            }
 
-    .close:hover,
-    .close:focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
-}
-
-</style>
-</head>
+            .close:hover,
+            .close:focus {
+                color: black;
+                text-decoration: none;
+                cursor: pointer;
+            }
+        </style>
+    </head>
 <body>
 
     <!-- Botão para abrir a modal -->
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,6 +18,8 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                    <a href="{{ route('modal.exemplo') }}"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Modal (Exemplo)</button></a>
+                   <a href="{{ url('endereco') }}"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Endereço (Exemplo)</button></a>
+                </div>
             </div>
         </div>        
     </div>
